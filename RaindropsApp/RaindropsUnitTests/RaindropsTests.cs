b5d45ac1bf30921cd0208a6e17ed0683087ce551
Ashib Hoque <a.hoque29@hotmail.com>
2021-03-27 20:26:26 +0000

    Initialised the test class for the raindrops class

---
+++ RaindropsApp/RaindropsUnitTests/RaindropsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RaindropFunctions;
+
+namespace RaindropsUnitTests
+{
+	[TestFixture]
+	[Parallelizable]
+	public class RaindropsTests
+	{
+		[Test]
+		public void WhenZeroIsPassed_ReturnsPlingPlangPlong()
+		{
+			Assert.That(Raindrops.PlingPlangPlong(0), Is.EqualTo("PlingPlangPlong"));
+		}
+	}
+}
