8de9825932f4f3081472d10e0d018208e4e3ba6c
Ashib Hoque <a.hoque29@hotmail.com>
2021-03-27 21:21:35 +0000

    Implemented the PlingPlangPlong() method.

--- RaindropsApp/RaindropsApp/Raindrops.cs
+++ RaindropsApp/RaindropsApp/Raindrops.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RaindropFunctions
+﻿namespace RaindropFunctions
 {
-	public class Raindrops
+	public static class Raindrops
 	{
+		public static string PlingPlangPlong(int n)
+		{
+			if (n % 3 != 0
+				&& n % 5 != 0
+				&& n % 7 != 0)
+			{
+				return n.ToString();
+			}
 
+			var output = "";
+
+			if (n % 3 == 0)
+			{
+				output += "Pling";
+			}
+
+			if (n % 5 == 0)
+			{
+				output += "Plang";
+			}
+
+			if (n % 7 == 0)
+			{
+				output += "Plong";
+			}
+
+			return output;
+		}
 	}
 }
